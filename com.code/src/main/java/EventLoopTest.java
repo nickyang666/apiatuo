fb8c55bede3bc0ecbbc74ce0211d48a36c38bdbe
= <=>
2023-02-06 14:07:21 +0800

    增加日志文件

---
+++ com.code/src/main/java/EventLoopTest.java
@@ -0,0 +1,15 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 循环
+ */
+@Slf4j
+public class EventLoopTest {
+
+    public static void main(String[] args) {
+
+        log.info("专注写bug！");
+    }
+}
+
+
