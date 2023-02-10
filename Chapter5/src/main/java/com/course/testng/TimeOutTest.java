a3b16b2a7c1296df797c8c2437fa8891b3c95340
= <=>
2023-02-10 09:08:42 +0800

    文件新增

--- Chapter5/src/main/java/com/course/testng/TimeOutTest.java
+++ Chapter5/src/main/java/com/course/testng/TimeOutTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
  * @time:2023/2/10 9:02
  */
 public class TimeOutTest {
-
+    //注释
     @Test(timeOut = 3000)//单位为毫秒值
     public void testSuccess() throws InterruptedException {
         Thread.sleep(2000);
